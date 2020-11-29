34f78252cbe9ffcfe52aa900a8f169c7cf4b9bcd
Sylvain <sylvain.gaborit@depinfonancy.net>
2020-11-29 22:58:11 +0100

    Two new games and a lot of cool features

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@
     */
     (function ($) {
     "use strict"; // Start of use strict
+	
 
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
@@ -104,25 +105,52 @@
 
 		//this line just stops it visiting the href which is always #
 		e.preventDefault();
-
-		var openedby = $(this).attr('data-o');
-		$('[data-ob="' + openedby +'"]').removeClass('off').addClass('on');
-		$('[data-obi="' + openedby +'"]').removeClass('off').addClass('on');
+		
+		if($(this).attr('class') == "on"){
+			var openedby = $(this).attr('data-o');
+			var closedby = $(this).attr('data-c');
+			
+			$(this).removeClass('on').addClass('off');
+			
+			if($('[data-ob="' + openedby +'"]').length !== 0){
+				$('[data-ob="' + openedby +'"]').removeClass('off').addClass('on');
+			}
+			if($('[data-obi="' + openedby +'"]').length !== 0){
+				$('[data-obi="' + openedby +'"]').removeClass('off').addClass('on');
+				$("html, body").animate(
+					{
+						scrollTop: $('[data-obi="' + openedby +'"]').offset().top - 72,
+					},
+					1000,
+					"easeInOutExpo"
+				);
+			}
+			
+			
+			if($('[data-cb="' + closedby +'"]').length !== 0){
+				$('[data-cb="' + closedby +'"]').remove();
+			}
+			
+			update();
+		}
 		
 
-		var closedby = $(this).attr('data-c');
-		$('[data-cb="' + closedby +'"]').remove();
+	  });
+	  
+	  //Show everything, for people that do not have time
+	  $('.show_all').click(function() {
+		$('[data-ob]').removeClass('off').addClass('on');
+		$('[data-obi]').removeClass('off').addClass('on');
+		cheatOnProgress();
+	  });
+	  
+	  //Hide everything, for people that finally realize it is not a matter of time, but to just enjoy the ride
+	  $('.hide_all').click(function() {
 
-		$(this).contents().unwrap();
-		
-		$("html, body").animate(
-            {
-                scrollTop: $('[data-obi="' + openedby +'"]').offset().top - 72,
-            },
-            1000,
-            "easeInOutExpo"
-        );
-
+		$('[data-ob]').removeClass('on').addClass('off');
+		$('[data-obi]').removeClass('on').addClass('off');
+		$('t[data-o]').removeClass('off').addClass('on');
+		resetProgress();
 	  });
 	  
 	  //The following is to make popup windows when click on projects to see details and download
@@ -141,9 +169,56 @@
 		$('.popupCloseButton').click(function(){
 			$('.hover_bkgr_fricc').hide();
 		});
+		
+		//The following is to apply the dark mode on click
+		$( "spec" ).on("click", function() { 
+            if( $( "body" ).hasClass( "dark" )) { 
+                $( "body" ).removeClass( "dark" ); 
+            } else { 
+                $( "body" ).addClass( "dark" ); 
+            }
+			if( $( ".hover_bkgr_fricc > div" ).hasClass( "dark" )) { 
+                $( ".hover_bkgr_fricc > div" ).removeClass( "dark" ); 
+            } else { 
+                $( ".hover_bkgr_fricc > div" ).addClass( "dark" );  
+            }
+			if($("#UnityImg").attr('src')=="assets/img/logos/unity-mwu-black.png"){
+				$("#UnityImg").attr("src", 'assets/img/logos/unity-mwu-white.png');
+			} else{
+				$("#UnityImg").attr("src", "assets/img/logos/unity-mwu-black.png");
+			}
+        }); 
+		
+		var progressI = 1;
+		var totalDiscover = $('t[data-o]').length + 1;
+		
+		function update(){
+			if(progressI<totalDiscover){
+				progressI++;
+				var percentI = progressI/totalDiscover;
+				move(Math.round(percentI*100));
+			}
+		}
+		function resetProgress(){
+			progressI = 0;
+			update();
+		}
+		function cheatOnProgress(){
+			progressI = totalDiscover-1;
+			update();
+		}
 	
 	
 })(jQuery); // End of use strict
+
+
+//Function to update the progress bar
+function move(percent) {
+    var elem = document.getElementById("TheBar");
+    var width = percent;
+    elem.style.width = width + "%";
+    elem.innerHTML = width + "%";
+}
 
 
 
